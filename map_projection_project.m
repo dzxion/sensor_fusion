cf5b1d8b78b887f20f2df34f19faff907fa186ca
dzxion <dzxion@qq.com>
2021-12-20 10:41:38 +0800

    motion accel compensate

---
+++ map_projection_project.m
@@ -0,0 +1,20 @@
+function [x,y,z] = map_projection_project( lat, lon, alt )
+%   lat : deg
+%   lon : deg
+%   alt : cm
+%	Date          Author         
+%	2021/11/16    Deng zhengxiong
+
+global ToRad ToDeg;
+rEarth = 637139300; % µØÇò°ë¾¶ cm
+lat = lat * ToRad;
+lon = lon * ToRad;
+lat_rad_distance = lat - lat(1);
+lon_rad_distance = lon - lon(1);
+x = lat_rad_distance * rEarth;
+x = x';
+y = lon_rad_distance.*(rEarth * cos(lat)); 
+y = y';
+z = alt - alt(1);
+z = -z';
+end

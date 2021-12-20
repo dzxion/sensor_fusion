ec05ed863113d3d904cb3ebdd9894219421292bd
dzxion <dzxion@qq.com>
2021-12-20 16:25:06 +0800

    对比加速度计经过运动加速度补偿后的角度

--- sensor_fusion.m
+++ sensor_fusion.m
@@ -11,9 +11,10 @@ load('航线飞行验证纯惯导and数据集_data.mat');
 %% 初始化
 deltat_imu = 0.004;deltat_gps = 0.2;
 imu_sys_time = imu_sys_time * 1e-3;
-gps_sys_time = gps_sys_time * 0.001;
+gps_sys_time = gps_sys_time * 1e-3;
 
-gravity = [0 0 -1];P_acc = 0.005;GravityAcc = 980.665;
+gravity = [0 0 -1];
+P_acc = 0.005;GravityAcc = 980.665;
 
 gps_location_lat = gps_location_lat * 1e-7;
 gps_location_lon = gps_location_lon * 1e-7;
@@ -35,6 +36,19 @@ acc_filter_x = filter(b,a,acc_x);
 acc_filter_y = filter(b,a,acc_y);
 acc_filter_z = filter(b,a,acc_z);
 
+%% 加速度算roll pitch
+for t=1:length(imu_sys_time)
+    acc_b = [acc_x(t) acc_y(t) acc_z(t)];
+    acc_b = acc_b / norm(acc_b);
+    acc_filter_b = [acc_filter_x(t) acc_filter_y(t) acc_filter_z(t)];
+    acc_filter_b = acc_filter_b / norm(acc_filter_b);
+    
+    pitch_acc(t) = asin(acc_b(1)) * ToDeg;
+    roll_acc(t) = atan2(-acc_b(2), -acc_b(3)) * ToDeg;
+    pitch_acc_filter(t) = asin(acc_filter_b(1)) * ToDeg;
+    roll_acc_filter(t) = atan2(-acc_filter_b(2), -acc_filter_b(3)) * ToDeg;
+end
+
 %% gps运动加速度补偿
 Qwb = [1 0 0 0];
 gps_count = 1;gps_update = false;imu_count = 0;
@@ -54,7 +68,7 @@ for t=1:length(imu_sys_time)
     if imu_count == 50
         while 1
             gps_count = gps_count + 1;
-            if gps_itow(gps_count) ~= gps_itow(gps_count-1) 
+            if gps_vitow(gps_count) ~= gps_vitow(gps_count-1) 
                 break;
             end
         end
@@ -78,6 +92,7 @@ for t=1:length(imu_sys_time)
     if gps_update
         acc_motion_w = (vel_gps - vel_gps_prev) / deltat_gps;
     end
+    acc_motion_gps(t,:) = acc_motion_w;
     acc_ref = acc_motion_w + gravity * GravityAcc;
     acc_ref = acc_ref / norm(acc_ref);
     [angle,axis] = get_included_angle_from_unit_vector(acc_w_filter.', acc_ref);
@@ -106,24 +121,52 @@ for t=1:length(imu_sys_time)
 end
 euler = quatern2euler(Qwb_hat) * ToDeg;
 
+%% 加速度算roll pitch（加入运动加速度补偿）
+for t=1:length(imu_sys_time)
+    acc_b = [acc_x(t) acc_y(t) acc_z(t)];
+    acc_b = acc_b / norm(acc_b);
+    acc_filter_b = [acc_filter_x(t) acc_filter_y(t) acc_filter_z(t)];
+    
+    Qwb = Qwb_hat(t,:);
+    Qbw = quaternConj(Qwb);
+    Rbw = quatern2rotMat(Qbw);
+    acc_motion = acc_motion_gps(t,:);
+    acc_motion = acc_motion.';
+    acc_motion_b = Rbw * acc_motion;
+    acc_motion_b = acc_motion_b.';
+    acc_compensate = acc_filter_b * GravityAcc - acc_motion_b;
+    acc_compensate = acc_compensate/norm(acc_compensate);
+    
+    pitch_acc_compensate(t) = asin(acc_compensate(1)) * ToDeg;
+    roll_acc_compensate(t) = atan2(-acc_compensate(2), -acc_compensate(3)) * ToDeg;
+end
+
 %% 姿态角对比
 figure
 subplot(311)
 plot(imu_sys_time, euler(:,1));
 grid on;hold on;
+plot(imu_sys_time, roll_acc_filter);
+grid on;hold on;
+plot(imu_sys_time, roll_acc_compensate);
+grid on;hold on;
 plot(imu_sys_time, roll);
 xlabel('time (s)');
 ylabel('angle (deg)');
-legend('roll','roll ref');
+legend('roll','roll acc','roll acc compensate','roll ref');
 title('Roll');
 
 subplot(312)
 plot(imu_sys_time, euler(:,2));
 grid on;hold on;
+plot(imu_sys_time, pitch_acc_filter);
+grid on;hold on;
+plot(imu_sys_time, pitch_acc_compensate);
+grid on;hold on;
 plot(imu_sys_time, Pitch);
 xlabel('time (s)');
 ylabel('angle (deg)');
-legend('pitch','pitch ref');
+legend('pitch','pitch acc','pitch acc compensate','pitch ref');
 title('Pitch');
 
 subplot(313)
